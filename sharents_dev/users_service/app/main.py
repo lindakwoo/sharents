39b851c4c1c25a337dad953e83f10a94643d5003
lindakwoo <lindakwoo@gmail.com>
2024-08-13 23:11:23 -0700

    change port back to 8001

--- sharents_dev/users_service/app/main.py
+++ sharents_dev/users_service/app/main.py
@@ -7,4 +7,4 @@ app.include_router(routes.router, prefix="", tags=["users"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
