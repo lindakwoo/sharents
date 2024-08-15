cba7c972ae8a9e4861190e359e020900738db8ab
Clarissa Lopez <lopezclarissa2021@gmail.com>
2024-08-15 14:42:23 -0700

    added some additional endpoints

--- journals/Clarissa.md
+++ journals/Clarissa.md
@@ -2,3 +2,11 @@
 
 Today  I worked on setting up each folder for the individual microservices in our project.
 As a team we did the endpoint for getting all the guardians and refactured files in each microservice.
+
+## Wednesday, August 14, 2024
+
+Today I worked on creating endpoints and testing them out in swagger.
+
+## Thursday, August 15, 2024
+
+Today I worked on refacturing endpoints, creating more endpoints and testing them out in swagger. Caleb dockerized our project, so I tested that to make sure it was up and running.
--- sharents_dev/milestones_media_service/app/utils.py
+++ sharents_dev/milestones_media_service/app/utils.py
@@ -18,11 +18,15 @@ def check_list_not_empty(data: List[T], error_message: str):
             status_code=status.HTTP_404_NOT_FOUND, detail=error_message)
     return data
 
-def convert_object_id(document: dict, id_field: str = "_id") -> dict:
-    """
-    Convert MongoDB _id to id and optionally remove _id.
-    """
-    if document and id_field in document:
-        document['id'] = str(document[id_field])
-        document.pop(id_field, None)
-    return document
+
+def check_update_result(update_result, error_message: str):
+    if update_result.modified_count == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=error_message
+        )
+
+
+def check_delete_result(result, error_message: str):
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail=error_message)
