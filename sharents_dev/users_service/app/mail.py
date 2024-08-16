99b3ded306aede4cb455ad7dc9dac3ebfe8bdca5
lindakwoo <lindakwoo@gmail.com>
2024-08-16 13:18:38 -0700

    add member and invite endpoints

---
+++ sharents_dev/users_service/app/mail.py
@@ -0,0 +1,32 @@
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema, MessageType
+from pathlib import Path
+import os
+
+
+BASE_DIR = Path(__file__).resolve().parent
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=os.getenv("MAIL_PORT"),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(BASE_DIR, 'templates')
+)
+
+mail = FastMail(
+    config=mail_config
+)
+
+mail = FastMail(config=mail_config)
+
+
+def create_message(recipients: list[str], subject: str, body: str):
+    message = MessageSchema(
+        recipients=recipients, subject=subject, body=body, subtype=MessageType.html)
+    return message
