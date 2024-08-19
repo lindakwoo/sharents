1a6eee3237f4e5ec86f1a905cc0a89da491d5186
lindakwoo <lindakwoo@gmail.com>
2024-08-19 12:41:49 -0700

    change routes

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from './components/Navbar';
 import Login from './components/forms/Login';
-import Home from "./components/Home";
+import Home from "./components/homepage/Home";
 import Landing from "./components/Landing";
 import './App.css';
 
