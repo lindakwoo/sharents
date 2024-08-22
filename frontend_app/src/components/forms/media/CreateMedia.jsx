0f11317e733e8c1ba476eb5dde02407d85cb9a7f
Clarissa Lopez <lopezclarissa2021@gmail.com>
2024-08-22 09:02:40 -0700

    starting media create

--- frontend_app/src/components/forms/media/CreateMedia.jsx
+++ frontend_app/src/components/forms/media/CreateMedia.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import customFetch from "../../../fetchWrapper";
+import { Box, styled } from "@mui/material";
+import { AuthContext } from "../../../context/AuthContext";
+
+const Button = styled("button")({});
 
 const CreateMedia = () => {
+  const [mediaData, setMediaData] = useState({ description: "", category: "", date: "", type: "", url: "", child: "" });
   return <div>CreateMedia</div>;
 };
 
