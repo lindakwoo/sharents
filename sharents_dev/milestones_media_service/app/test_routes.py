fb2443689e5543b854ff063f891fc39423e61746
Clarissa Lopez <lopezclarissa2021@gmail.com>
2024-08-28 10:11:02 -0700

    added test to post a media for a child

--- sharents_dev/milestones_media_service/app/test_routes.py
+++ sharents_dev/milestones_media_service/app/test_routes.py
@@ -59,3 +59,56 @@ async def test_post_comment_on_milestone_success(monkeypatch, test_app):
     # Assertions to check if the response is as expected
     assert response.status_code == 200
     assert response_json == mock_find_one_data
+
+
+@pytest.mark.asyncio
+async def test_post_media_for_a_child_success(monkeypatch, test_app):
+    # mock media data to be inseted
+    mock_media_data = {
+        "description": "string",
+        "category": "growth",
+        "date": "string",
+        "type": "string",
+        "url": "string"
+    }
+
+    # mock data returned by the find_one method, resembling the MediaModel
+    mock_find_one_data = {
+        "id": None,
+        "description": "string",
+        "category": "growth",
+        "child": "test_child_id",
+        "date": "string",
+        "type": "string",
+        "url": "string",
+        "created_at": "string",
+    }
+
+    # mock database insert and find operations
+    async def mock_insert_one(self, document):
+        class MockInsertResult:
+            inserted_id = "test_inserted_id"
+        return MockInsertResult()
+
+    async def mock_find_one(self, query):
+        return mock_find_one_data
+
+    # apply the monkey patch
+    monkeypatch.setattr(
+        "app.database.db.get_collection",
+        lambda collection_name: type(
+            "", (), {"insert_one": mock_insert_one, "find_one": mock_find_one})()
+    )
+
+    test_child_id = "test_child_id"
+    url = f"/api/media/children/{test_child_id}/"
+
+    # create a test client
+    async with AsyncClient(app=test_app, base_url="http://test") as ac:
+        response = await ac.post(url, json=mock_media_data)
+
+    response_json = response.json()
+
+    # assertions to check if the response is as expected
+    assert response.status_code == 200
+    assert response_json == mock_find_one_data
