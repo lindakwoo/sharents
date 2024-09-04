007f4fa9f76e86233a9dcbc1ecf1b028a5b970ae
Caleb Pierre <calebsaunders@gmail.com>
2024-09-04 13:20:10 -0700

    fixed guardian_id

--- sharents_dev/users_service/app/schemas.py
+++ sharents_dev/users_service/app/schemas.py
@@ -43,10 +43,10 @@ class UserModel(UserBase):
     role: str = Field(...)  # The role of the user (e.g., 'guardian' or 'member')
     member: Optional[MemberModel] = None  # Optional member data for the user
     guardian_id: Optional[PyObjectId] = Field(
-        alias="_guardian_id", default=None
+        alias="guardian_id", default=None
     )  # ID of the guardian
     member_id: Optional[PyObjectId] = Field(
-        alias="_member_id", default=None
+        alias="member_id", default=None
     )  # ID of the member
 
 
@@ -83,7 +83,7 @@ class GuardianModel(UserModel):
     """Model representing a guardian stored in the database."""
 
     id: Optional[PyObjectId] = Field(alias="_id", default=None)  # MongoDB ObjectId
-    guardian_id: Optional[str]
+
     # Guardian-specific fields can be added here if needed
 
 
