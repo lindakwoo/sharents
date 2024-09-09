9f9d4aed0e7dcf1d6a6b4f315b2614675d3b1466
lindakwoo <lindakwoo@gmail.com>
2024-09-09 16:17:05 -0700

    add Depends

--- sharents_dev/guardians_service/app/routes.py
+++ sharents_dev/guardians_service/app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from typing import List
 
 
@@ -34,6 +34,7 @@ async def create_guardian_route(guardian: GuardianModel):
     "/guardians/{guardian_id}",
     response_model=GuardianModel,
     response_model_by_alias=False,
+
 )
 async def get_guardian_route(guardian_id: str):
     """
